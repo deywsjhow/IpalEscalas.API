52e2d7739ed56e77feb338c36a1711c5555cb5fa
deywsjhow <jonathan.deyws@aluno.faculdadeimpacta.com.br>
2023-11-07 22:44:07 -0300

    tirando password da response authentication

--- PortalIpalEscalas.Common/Models/AuthResponse.cs
+++ PortalIpalEscalas.Common/Models/AuthResponse.cs
@@ -8,7 +8,6 @@ namespace PortalIpalEscalas.Common.Models
     {
         public int userId { get; set; }
         public string user { get; set; }
-        public string password { get; set; }
         public string email { get; set; }
         public string attribuation { get; set; }
         public int loginType { get; set; }
